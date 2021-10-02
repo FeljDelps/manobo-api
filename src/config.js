816e59e24506cd7a4af2467a662fc6ef7a728c4a
Felj Abas <felabas41@gmail.com>
2021-10-02 15:45:47 -0700

    prepared for deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8001,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://manobo_admin@localhost/manobo' 
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://manobo_admin@localhost/manobo',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://manobo_admin@localhost/manobo-test'
 };
